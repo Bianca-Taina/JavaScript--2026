1d9d40afb14bdc99dd48d2666c19add135f7285b
Bianca Tainá Lino Balduino <bianca.balduino@escola.pr.gov.br>
2026-02-09 13:05:40 +0000

    acabei

---
+++ calculaMedia.js
@@ -0,0 +1,12 @@
+let nota1 = 8;
+let nota2 = 7;
+let nota3 = 6;
+let nota4 = 5;
+
+const nomeAluno = "Beatriz";
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log("O aluno: " + nomeAluno);
+console.log("Tem a média: " + media);
+
