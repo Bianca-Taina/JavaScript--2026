840e45cd3d014699a8d160a7794446b3ca975c3a
Bianca Tainá Lino Balduino <bianca.balduino@escola.pr.gov.br>
2026-02-09 17:30:39 +0000

    acabei

--- calculaMedia.js
+++ calculaMedia.js
@@ -1,12 +1,22 @@
-let nota1 = 8;
-let nota2 = 7;
-let nota3 = 6;
-let nota4 = 5;
+let nota1 = 9;
+let nota2 = 6;
+let nota3 = 8;
+let nota4 = 7;
 
 const nomeAluno = "Beatriz";
 
 let media = (nota1 + nota2 + nota3 + nota4) / 4
 
-console.log("O aluno: " + nomeAluno);
-console.log("Tem a média: " + media);
+if (media >= 7){
+    console.log("A Aluna " + nomeAluno + " está aprovado")
+} 
+if (media >= 5 && media < 7){
+    console.log("A Aluna " + nomeAluno + " está de se liga")
+}
+if (media < 5){
+     console.log("A Aluna " + nomeAluno + " está reprovado")
+}     
+
+// console.log("O aluno: " + nomeAluno);
+// console.log("Tem a média: " + media);
 
