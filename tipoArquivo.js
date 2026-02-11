26bdd9509e9c03bd183773b2cd045a56009ea440
Bianca Tainá Lino Balduino <bianca.balduino@escola.pr.gov.br>
2026-02-11 13:04:44 +0000

    salvar

---
+++ tipoArquivo.js
@@ -0,0 +1,9 @@
+testeString = "50"
+testeint = 50
+testeFloat = 50.0
+testeBoolean = true
+
+console.log(typeof(testeString));
+console.log(typeof(testeint));
+console.log(typeof(testeFloat));
+console.log(typeof(testeBoolean));
